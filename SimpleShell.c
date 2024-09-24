6125c9c79938268434a4114b9cf23566efdb5481
Bhavikgarg302005 <147907427+Bhavikgarg302005@users.noreply.github.com>
2024-09-24 23:02:11 +0530

    Update SimpleShell.c

--- SimpleShell.c
+++ SimpleShell.c
@@ -7,14 +7,18 @@
 #include <signal.h>
 #include <time.h> 
 #include <ctype.h> 
+#include <readline/readline.h>
+#include <readline/history.h>
 
-#define MAX_COMMANDS 10
+
+#define MAX_COMMANDS 100
 #define MAX_ARGS 100
 
 typedef struct {
     char* cmd;
     int pid;
     time_t initial_time;
+    int backgrnd;
     double duration;
 } history;
 
@@ -25,12 +29,27 @@ void histry() {
     for (int i = 0; i < cntr; i++) {
         printf("Given command-%d was: %s\n", i + 1, hst[i].cmd);
         printf("Starting time for process: %s", ctime(&hst[i].initial_time));
+        if(hst[i].backgrnd==1){
+            printf("Background process\n");
+        }
         printf("Process pid is: %d\n", hst[i].pid);
         printf("Process duration is: %f seconds\n", hst[i].duration);
         printf("\n");
     }
 }
+void remove_character(char *str, char char_to_remove) {
+    int i, j = 0;
+    int len = strlen(str);
+    char result[len + 1]; 
 
+    for (i = 0; i < len; i++) {
+        if (str[i] != char_to_remove) {
+            result[j++] = str[i];
+        }
+    }
+    result[j] = '\0'; 
+    strcpy(str, result);
+}
 int create_and_run(char *command) {
     clock_t start_time = clock();
     int child_st = fork();
@@ -39,14 +58,19 @@ int create_and_run(char *command) {
     
     if (child_st < 0) {
         perror("Fork failed");
-        exit(EXIT_FAILURE);
+        exit(0);
+    }
+    int background=0;
+    if (strchr(command, '&')) {
+        background = 1;
+        remove_character(command, '&');
     }
     
     if (child_st == 0) {
         char *argv[1024];
         char *temp = strtok(command, " ");
         if (temp == NULL) {
-            fprintf(stderr, "Error in tokenising string\n");
+            printf("Error in tokenising string\n");
             exit(EXIT_FAILURE);
         }
         int pos = 0;
@@ -55,21 +79,27 @@ int create_and_run(char *command) {
             temp = strtok(NULL, " ");
         }
         argv[pos] = NULL;
-
         if (execvp(argv[0], argv) == -1) {
-            perror("Exec error");
+            printf("Exec error\n");
             exit(EXIT_FAILURE);
         }
+        exit(1);
     } else {
         int status;
-        if (waitpid(child_st, &status, 0) == -1) {
-            perror("Wait failed");
+        if(!background){
+          if (waitpid(child_st, &status, 0) == -1) {
+              printf("Wait failed\n");
+              exit(EXIT_FAILURE);
+            }
         }
         clock_t end_time = clock();
         hst[cntr].cmd = malloc(strlen(command) + 1);
         if (hst[cntr].cmd == NULL) {
-            perror("Error in allocating size");
+            printf("Error in allocating size\n");
             exit(EXIT_FAILURE);
+        }
+        if(background){
+            hst[cntr].backgrnd=1;
         }
         strcpy(hst[cntr].cmd, command);
         hst[cntr].pid = child_st;
@@ -81,13 +111,38 @@ int create_and_run(char *command) {
 }
 
 int launch(char *command) {
+    if (strncmp(command, "cd", 2) == 0) {
+        clock_t start_time = clock();
+        time_t initial_time;
+        time(&initial_time);
+        char *dir = strtok(command + 3, " ");
+        if (dir == NULL) {
+            dir = getenv("HOME");
+        }
+        if (chdir(dir) != 0) {
+            printf("cd failed\n");
+            exit(EXIT_FAILURE);
+        }
+        hst[cntr].cmd = malloc(strlen(command) + 1);
+        if (hst[cntr].cmd == NULL) {
+            printf("Error in allocating size\n");
+            exit(EXIT_FAILURE);
+        }
+        strcpy(hst[cntr].cmd, command);
+        hst[cntr].pid = getpid();
+        hst[cntr].initial_time = initial_time;
+        clock_t end_time = clock();
+        hst[cntr].duration = ((double)(end_time - start_time) / CLOCKS_PER_SEC);
+        cntr++;
+        return 1;
+    } 
     if (strcmp(command, "history") == 0) { 
         clock_t start_time = clock();
         time_t initial_time;
         time(&initial_time);
         hst[cntr].cmd = malloc(strlen(command) + 1);
         if (hst[cntr].cmd == NULL) {
-            perror("Error in allocating size");
+            printf("Error in allocating size\n");
             exit(EXIT_FAILURE);
         }
         strcpy(hst[cntr].cmd, command);
@@ -107,42 +162,55 @@ int launch(char *command) {
 }
 
 void my_handler(int signum) {
-    printf("Exited Successfully \n");
-    histry();
-    exit(0);
+    if(signum==SIGINT){
+       printf("Exited Successfully \n");
+       histry();
+       exit(0);
+    }
+    if(signum==SIGCHLD){
+        while((waitpid(-1, NULL, WNOHANG) > 0));
+    }
 }
 
 void slice(char *s, char *t, int st, int en) {
     if (st <= en) {
         int o = 0;
         for (int i = st; i <= en; ++i) {
-            t[o++] = s[i];
+            t[o] = s[i];
+            o++;
         }
         t[o] = '\0';
     }
 }
 
 void pipeC(char *i) {
-    char *cm = malloc(strlen(i) + 1);
+    int background=0;
+    if (strchr(i, '&')) {
+        background = 1;
+    
+       remove_character(i, '&');
+    }
+    char *cmdd = malloc(strlen(i)+1);
+    strcpy(cmdd,i);
+    char *cm = strtok(i, "|");
     if (cm == NULL) {
-        perror("Error in allocating space");
+        printf("Error in allocating space");
         exit(EXIT_FAILURE);
     }
-    strcpy(cm, i);
     time_t start_time=clock();
     
     char *commands[MAX_COMMANDS];
     int num_commands = 0;
     
-    char *cmd = strtok(cm, "|");
-    while (cmd != NULL && num_commands < MAX_COMMANDS) {
-        commands[num_commands] = malloc(strlen(cmd) + 1);
+    //char *cmd = strtok(cm, "|");
+    while (cm != NULL && num_commands < MAX_COMMANDS) {
+        commands[num_commands] = (char*)malloc(strlen(cm) + 1);
         if (commands[num_commands] == NULL) {
-            perror("Error in allocating command space");
+            printf("Error in allocating command space");
             exit(EXIT_FAILURE);
         }
-        strcpy(commands[num_commands], cmd);
-        cmd = strtok(NULL, "|");
+        slice(cm,commands[num_commands],0,strlen(cm)-1);
+        cm = strtok(NULL, "|");
         num_commands++;
     }
 
@@ -150,7 +218,7 @@ void pipeC(char *i) {
 
     for (int i = 0; i < num_commands - 1; i++) {
         if (pipe(pipe_fd + i * 2) == -1) {
-            perror("Pipe creation failed");
+            printf("Pipe creation failed\n");
             exit(EXIT_FAILURE);
         }
     }
@@ -174,18 +242,17 @@ void pipeC(char *i) {
             }
 
             char *args[MAX_ARGS];
-            char *cmd = commands[i];
-            char *arg = strtok(cmd, " ");
+            char *cmd1 = commands[i];
+            cm= strtok(cmd1, " ");
             int arg_count = 0;
 
-            while (arg != NULL) {
-                args[arg_count++] = arg;
-                arg = strtok(NULL, " ");
+            while (cm != NULL) {
+                args[arg_count++] = cm;
+                cm= strtok(NULL, " ");
             }
             args[arg_count] = NULL; 
-
             if (execvp(args[0], args) == -1) {
-                perror("Execvp error");
+                printf("Execvp error\n");
                 exit(EXIT_FAILURE);
             }
         }
@@ -194,19 +261,27 @@ void pipeC(char *i) {
     for (int i = 0; i < 2 * (num_commands - 1); i++) {
         close(pipe_fd[i]);
     }
-
-    for (int i = 0; i < num_commands; i++) {
-        wait(NULL);
+    int status;
+    if(!background){
+     for (int i = 0; i < num_commands-1; i++) {
+          if (wait(NULL) == -1) {
+              printf("Wait failed\n");
+              exit(EXIT_FAILURE);
+            }
+     }
     }
 
     clock_t end_time = clock();
-    hst[cntr].cmd = malloc(strlen(i) + 1);
+    hst[cntr].cmd = malloc(strlen(cmdd) + 1);
     if (hst[cntr].cmd == NULL) {
-        perror("Error in allocating size");
+        printf("Error in allocating size\n");
         exit(EXIT_FAILURE);
     }
-    strcpy(hst[cntr].cmd, i);
+    strcpy(hst[cntr].cmd, cmdd);
     hst[cntr].pid = pid;
+    if(background){
+        hst[cntr].backgrnd=1;
+    }
     hst[cntr].initial_time = time(NULL);
     hst[cntr].duration = ((double)(end_time - start_time) / CLOCKS_PER_SEC);
     cntr++; 
@@ -229,18 +304,24 @@ int main() {
     memset(&sig, 0, sizeof(sig)); 
     sig.sa_handler = my_handler;
     if (sigaction(SIGINT, &sig, NULL) == -1) {
-        perror("sigaction error");
+        printf("sigaction error\n");
         exit(EXIT_FAILURE);
     }
-    
-    do {
-        printf("gaur-garg-iiitd.ac: ");
-        char command[500];
-        if (fgets(command, sizeof(command), stdin) == NULL) { 
-            perror("fgets error");
+    if(sigaction(SIGCHLD,&sig,NULL)== -1){
+        printf("sigaction error\n");
+        exit(EXIT_FAILURE);
+    }
+    do{
+        char* command;
+        command=malloc(500);
+        if(command==NULL){
+            printf("Allocation not done\n");
             exit(EXIT_FAILURE);
         }
-
+        command=readline("gaur-garg-iiitd.ac:");
+        if(command[0] == '\0'){
+            continue;
+        }
         int bool = 0;
         int len = strlen(command);
         for (int i = 0; i < len; i++) {
@@ -257,8 +338,12 @@ int main() {
             }
             status = launch(command);
         }
+
+        if(command){
+            free(command);
+        }
         
-    } while (status);
+    }while (status);
     
     printf("Completed shell process\n");
     return 0;
